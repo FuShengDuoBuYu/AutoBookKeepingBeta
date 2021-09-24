3ce57213dcf9ebbb4795ee2b428c2212ed320723
郭仲天 <19307110250@fudan.edu.cn>
2021-09-24 13:02:22 +0800

    小组件成功更新

--- AutoBookKeeping/app/src/main/java/com/beta/autobookkeeping/OrderWidget.java
+++ AutoBookKeeping/app/src/main/java/com/beta/autobookkeeping/OrderWidget.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import Util.Util;
+
 public class OrderWidget extends AppWidgetProvider {
     /**
      * 接收窗口小部件点击时发送的广播
@@ -16,6 +18,9 @@ public class OrderWidget extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
+//        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.order_widget);
+//        remoteViews.setTextViewText(R.id.wtvAllTodayOrder, String.valueOf(Util.getTodayMoney(context)));
+//        remoteViews.setTextViewText(R.id.wtvAllMonthOrder, String.valueOf(Util.getMonthMoney(context)));
     }
     /**
      * 每次窗口小部件被更新都调用一次该方法
@@ -23,13 +28,17 @@ public class OrderWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d("tag", String.valueOf(Util.getMonthMoney(context)));
 //        Log.i("AppWidget", "开始了更新");
-//        RemoteViews rv = new RemoteViews(AppUMS.mContent.getPackageName(), R.layout.my_app_widget);
+//        RemoteViews rv = new RemoteViews(AppUMS.mContent.getPackageName(), R.layout.order_widget);
 ////这里获得当前的包名，并且用AppWidgetManager来向NewAppWidget.class发送广播。
 //        AppWidgetManager manager = AppWidgetManager.getInstance(AppUMS.mContent);
 //        ComponentName cn = new ComponentName(AppUMS.mContent, NewAppWidget.class);
 //        manager.updateAppWidget(cn, rv);
-//    }
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.order_widget);
+        remoteViews.setTextViewText(R.id.wtvAllTodayOrder, String.valueOf(Util.getTodayMoney(context)));
+        remoteViews.setTextViewText(R.id.wtvAllMonthOrder, String.valueOf(Util.getMonthMoney(context)));
+        appWidgetManager.updateAppWidget(appWidgetIds,remoteViews);
     }
     /**
      * 每删除一次窗口小部件就调用一次
@@ -38,7 +47,7 @@ public class OrderWidget extends AppWidgetProvider {
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
         //context.stopService(new Intent(context, WidgetService.class));
-        Log.i("AppWidget", "删除成功！");
+//        Log.i("AppWidget", "删除成功！");
     }
     /**
      * 当该窗口小部件第一次添加到桌面时调用该方法
@@ -46,9 +55,13 @@ public class OrderWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
+//        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.order_widget);
+//        remoteViews.setTextViewText(R.id.wtvAllTodayOrder, String.valueOf(Util.getTodayMoney(context)));
+//        remoteViews.setTextViewText(R.id.wtvAllMonthOrder, String.valueOf(Util.getMonthMoney(context)));
+//            Log.d("tag", String.valueOf(Util.getMonthMoney(context)));
         // Intent mTimerIntent = new Intent(context, WidgetService.class);
         // context.startService(mTimerIntent);
-        Log.i("AppWidget", "创建成功！");
+//        Log.i("AppWidget", "创建成功！");
     }
     /**
      * 当最后一个该窗口小部件删除时调用该方法
@@ -58,7 +71,7 @@ public class OrderWidget extends AppWidgetProvider {
         super.onDisabled(context);
         //  Intent mTimerIntent = new Intent(context, WidgetService.class);
         // context.stopService(mTimerIntent);
-        Log.i("AppWidget", "删除成功！");
+//        Log.i("AppWidget", "删除成功！");
     }
     /**
      * 当小部件大小改变时
